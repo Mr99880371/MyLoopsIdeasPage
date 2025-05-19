eec998aab9bdc5facd7776f70d21745a8c9cd8e5
Mr99880371 <mariane_araujo@id.uff.br>
2025-05-19 14:57:39 -0300

    ARC: new architeture add in project

--- src/Components/Footer/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const FooterContainer = styled.footer`
-  display: flex;
-  background-color: #eaeff5;
-  justify-content: center;
-  align-items: center;
-  height: 100px;
-`;
